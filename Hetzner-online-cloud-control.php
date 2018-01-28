d0cc5f4194ec9ece2a59333ae727e6a9279cbf7e
baekerIT <baeker-1T>
2018-01-28 15:06:15 +0100

    First Alpha
    Including a basic Server Overview and Server Creation

---
+++ Hetzner-online-cloud-control.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+Plugin Name: Hetzner Online Cloud Control
+Plugin URI: https://baeker-it.de
+Description: Take the full Control of your Hetzner Cloud!
+Version: 1.0
+Author: Hendrik Bäker
+Author URI: http://baeker-it.de
+License: GPL2
+*/
+
+require_once plugin_dir_path( __FILE__ ) . 'app/Hetzner_Cloud_Control_Remote_API.php';
+
+class baekerIT_Hetzner_Cloud_Control extends Hetzner_Cloud_Control_Remote_API {
+
+	const API_TOKEN = 'baekerit_hetzner_cloud_api_token';
+
+	public function __construct() {
+		if ( get_option( self::API_TOKEN ) != null ) {
+			add_action( 'admin_menu', [ $this, 'admin_menus' ] );
+			add_action( 'wp_ajax_createNewServer' . [ $this, 'hcc_createNewServer' ] );
+		} else {
+			add_action( 'admin_menu', [ $this, 'setup_menu' ] );
+		}
+		if ( is_admin() ) {
+			wp_enqueue_style( 'bootstrap', plugin_dir_path( __FILE__ ) . 'assets/css/bootstrap.min.css' );
+			wp_enqueue_script( 'bootstrap_js', plugin_dir_path( __FILE__ ) . 'assets/js/bootstrap.min.js' );
+		}
+	}
+
+	public function admin_menus() {
+		add_menu_page( 'Hetzner Cloud Control Overview', 'Hetzner Cloud Control', 'manage_options', 'bit_hcc', [
+			$this,
+			'hcc_overview'
+		] );
+		add_submenu_page( 'bit_hcc', 'Server Overview', 'Servers', 'manage_options', 'bit_hcc_servers', [
+			$this,
+			'hcc_serverView'
+		] );
+	}
+
+	public function setup_menu() {
+		add_menu_page( 'Hetzner Cloud Control Installation', 'Hetzner Cloud Control', 'manage_options', 'bit_hcc_setup', [
+			$this,
+			'init_setup'
+		] );
+	}
+
+	public function hcc_serverView() {
+		include plugin_dir_path( __FILE__ ) . 'resources/views/servers/getAllServers.phtml';
+	}
+
+	public function hcc_createNewServer() {
+		$name = filter_input( INPUT_POST, 'name' );
+		$server_type = filter_input( INPUT_POST, 'type' );
+		$image = filter_input( INPUT_POST, 'image' );
+
+		return Hetzner_Cloud_Control_Remote_API::createNewServer( $name, $server_type, $image );
+	}
+}
+
+add_action( 'init', 'loadHcc' );
+
+function loadHcc() {
+	return new baekerIT_Hetzner_Cloud_Control();
+}
---
+++ app/Hetzner_Cloud_Control_Remote_API.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hendrik
+ * Date: 28.01.18
+ * Time: 13:47
+ */
+
+class Hetzner_Cloud_Control_Remote_API {
+
+	const TOKEN = 'ZD1hjuIZY6ccboLIMmB7zV7yu2Xor0LxFm2yZIuXhDexFXb1x50il7AUsvCCESyx';
+	static $curl;
+	static protected $url = 'https://api.hetzner.cloud/v1';
+
+	public static function getAllServers() {
+		self::$curl = curl_init();
+		curl_setopt( self::$curl, CURLOPT_URL, self::$url . '/servers' );
+		curl_setopt( self::$curl, CURLOPT_HEADER, [ 'Authorization: Bearer ' . self::TOKEN ] );
+		$response = curl_exec( self::$curl );
+
+		return json_decode( $response );
+	}
+
+	public static function getAllServerTypes() {
+		self::$curl = curl_init();
+		curl_setopt( self::$curl, CURLOPT_URL, self::$url . '/server_types' );
+		curl_setopt( self::$curl, CURLOPT_HEADER, [ 'Authorization: Bearer ' . self::TOKEN ] );
+		$response = curl_exec( self::$curl );
+
+		return json_decode( $response );
+	}
+
+	public static function getAllImages() {
+		self::$curl = curl_init();
+		curl_setopt( self::$curl, CURLOPT_URL, self::$url . '/images' );
+		curl_setopt( self::$curl, CURLOPT_HEADER, [ 'Authorization: Bearer ' . self::TOKEN ] );
+		$response = curl_exec( self::$curl );
+
+		return json_decode( $response );
+	}
+
+	public static function createNewServer( $name, $type, $image ) {
+		self::$curl = curl_init();
+		curl_setopt( self::$curl, CURLOPT_URL, self::$url . '/servers' );
+		curl_setopt( self::$curl, CURLOPT_POST, true );
+		curl_setopt( self::$curl, CURLOPT_POSTFIELDS, [
+			'name'  => $name,
+			'type'  => $type,
+			'image' => $image
+		] );
+		curl_setopt( self::$curl, CURLOPT_HEADER, [ 'Authorization: Bearer ' . self::TOKEN ] );
+		$response = curl_exec( self::$curl );
+
+		return json_decode( $response );
+	}
+
+}
